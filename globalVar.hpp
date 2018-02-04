228e39249a312682def93751d5846c0486d9287d
Kentwh <kent.wincent46@gmail.com>
2018-02-04 15:20:17 +0100

    Updated .gitignore to ignore CMake files

--- globalVar.hpp
+++ globalVar.hpp
@@ -1,1 +1,5 @@
-uint score;
+#include "structs.hpp"
+
+unsigned int score;
+const std::string textureFile = "./assets/pacman.png";
+const std::string mapFile = "./levels/level0";
---
+++ pacMan.cpp
@@ -0,0 +1,8 @@
+#include "globalVar.hpp"
+#include "struct.hpp"
+#include "template.hpp"
+
+int main()
+{
+    return 0;
+}
