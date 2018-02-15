b20253530412c0003f74ec01f2c3ffbd9d1f75a6
Kent Holt <kentwh@stud.ntnu.no>
2018-02-15 22:54:01 +0100

    Update template.hpp: Added a TODO reminder comment for a possible solution to fix broken walking

--- template.hpp
+++ template.hpp
@@ -8,4 +8,7 @@ glm::vec4 collision(T obj)
                      (w.map[int((j + 1) * w.size.x + (i    ))].z == 1),
                      (w.map[int((j    ) * w.size.x + (i + 1))].z == 1),
                      (w.map[int((j - 1) * w.size.x + (i    ))].z == 1));
+    // TODO: 
+    // - use found i and j to look up actual world position within w.map, and check agains pacmans entire body collides with something, not just if the next tile is a wall
+    // - Make function take in object to check agains, maybe also the xy size of the object
 }
