499e815a4db0e79d4819f778a07d621e6a7de5ea
Zohaib Butt <zohaibb@stud.ntnu.no>
2018-02-15 10:25:13 +0100

    Texture coordination work under progress

--- pacMan.cpp
+++ pacMan.cpp
@@ -29,107 +29,107 @@ GLuint shaderProgram;
 GLuint textureShaderProg;
 
 void readFile() {
-    std::ifstream inputFile;
-    inputFile.open("levels/level0");
-    unsigned int x;
-    inputFile >> w.size.x; inputFile.ignore();
-    inputFile >> w.size.y;
+	std::ifstream inputFile;
+	inputFile.open("levels/level0");
+	unsigned int x;
+	inputFile >> w.size.x; inputFile.ignore();
+	inputFile >> w.size.y;
 
-    while (!inputFile.eof()) {
-        // read the map data from file
-        for (int i = 0; i < w.size.y; i++) {
-            for (int j = 0; j < w.size.x; j++) {
-                inputFile >> x;
-                w.map.push_back(glm::vec3((j * (2 / w.size.x) - 1), (1 - i * (2 / w.size.y)), x));
+	while (!inputFile.eof()) {
+		// read the map data from file
+		for (int i = 0; i < w.size.y; i++) {
+			for (int j = 0; j < w.size.x; j++) {
+				inputFile >> x;
+				w.map.push_back(glm::vec3((j * (2 / w.size.x) - 1), (1 - i * (2 / w.size.y)), x));
 
-                // Find tile of type 2 and set it to pacmans starting pos
-                if (x == 2)
-                    pm.position = glm::vec2((j * (2 / w.size.x) - 1), (1 - i * (2 / w.size.y)));
-            }
-            inputFile.ignore();
-        }
-    }
-    inputFile.close();
+				// Find tile of type 2 and set it to pacmans starting pos
+				if (x == 2)
+					pm.position = glm::vec2((j * (2 / w.size.x) - 1), (1 - i * (2 / w.size.y)));
+			}
+			inputFile.ignore();
+		}
+	}
+	inputFile.close();
 }
 
 void setupOpengl() {
-    try {
-        if (!glfwInit()) {	// Initialize GLFW
-            throw 1;
-        }
-    }
-    catch (int e) {
-        std::cout << "Failed to initialize GLFW! Exception nr: " << e << '\n';
-    }
-    // Window data
-    glfwWindowHint(GLFW_SAMPLES, 16);
-    glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
-    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); //	MacOs. 
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); // Old OpenGL is not acceptable.
+	try {
+		if (!glfwInit()) {	// Initialize GLFW
+			throw 1;
+		}
+	}
+	catch (int e) {
+		std::cout << "Failed to initialize GLFW! Exception nr: " << e << '\n';
+	}
+	// Window data
+	glfwWindowHint(GLFW_SAMPLES, 16);
+	glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); //	MacOs. 
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); // Old OpenGL is not acceptable.
 
-    window = glfwCreateWindow(HEIGHT, WIDTH, "Pacman", NULL, NULL);
-    try {
-        if (window == NULL) {
-            glfwTerminate();
-            throw 2;
-        }
-    }
-    catch (int e) {
-        std::cout << "Failed to open window! Exception nr: " << e << '\n';
-    }
-    glfwMakeContextCurrent(window);
-    glewExperimental = GL_TRUE;
-    try {
-        // Initialize GLEW
-        if (glewInit() != GLEW_OK) {
-            getchar();
-            glfwTerminate();
-            throw 3;
-        }
-    }
-    catch (int e) {
-        std::cout << "Failed to initialize GLEW! Exception nr: " << e << '\n';
-    }
-    glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
-    glfwSwapInterval(1);
+	window = glfwCreateWindow(HEIGHT, WIDTH, "Pacman", NULL, NULL);
+	try {
+		if (window == NULL) {
+			glfwTerminate();
+			throw 2;
+		}
+	}
+	catch (int e) {
+		std::cout << "Failed to open window! Exception nr: " << e << '\n';
+	}
+	glfwMakeContextCurrent(window);
+	glewExperimental = GL_TRUE;
+	try {
+		// Initialize GLEW
+		if (glewInit() != GLEW_OK) {
+			getchar();
+			glfwTerminate();
+			throw 3;
+		}
+	}
+	catch (int e) {
+		std::cout << "Failed to initialize GLEW! Exception nr: " << e << '\n';
+	}
+	glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
+	glfwSwapInterval(1);
 
-    // map vertices
-    glm::vec2* vertices_position;
-    glm::vec4* vertices_color;
+	// map vertices
+	glm::vec2* vertices_position;
+	glm::vec4* vertices_color;
 
-    glm::vec4 tileColor;
+	glm::vec4 tileColor;
 
-    vertices_position = new glm::vec2[w.size.x * w.size.y * 6];
-    vertices_color = new glm::vec4[w.size.x * w.size.y * 6];
+	vertices_position = new glm::vec2[w.size.x * w.size.y * 6];
+	vertices_color = new glm::vec4[w.size.x * w.size.y * 6];
 
-    for (size_t i = 0; i < w.size.x * w.size.y; i++)
-    {
-        tileColor = ((w.map[i].z < 1) ? glm::vec4(0.98f, 0.15f, 0.45f, 1.0f) : glm::vec4(0.0f, 0.0f, 0.0f, 0.0f));
+	for (size_t i = 0; i < w.size.x * w.size.y; i++)
+	{
+		tileColor = ((w.map[i].z < 1) ? glm::vec4(0.98f, 0.15f, 0.45f, 1.0f) : glm::vec4(0.0f, 0.0f, 0.0f, 0.0f));
 
-        vertices_position[6 * i + 0] = glm::vec2(w.map[i].x, w.map[i].y);
-        vertices_color[6 * i + 0] = tileColor;
+		vertices_position[6 * i + 0] = glm::vec2(w.map[i].x, w.map[i].y);
+		vertices_color[6 * i + 0] = tileColor;
 
-        vertices_position[6 * i + 1] = glm::vec2(w.map[i].x, w.map[i].y - (2 / w.size.y));
-        vertices_color[6 * i + 1] = tileColor;
+		vertices_position[6 * i + 1] = glm::vec2(w.map[i].x, w.map[i].y - (2 / w.size.y));
+		vertices_color[6 * i + 1] = tileColor;
 
-        vertices_position[6 * i + 2] = glm::vec2(w.map[i].x + (2 / w.size.x), w.map[i].y);
-        vertices_color[6 * i + 2] = tileColor;
+		vertices_position[6 * i + 2] = glm::vec2(w.map[i].x + (2 / w.size.x), w.map[i].y);
+		vertices_color[6 * i + 2] = tileColor;
 
-        vertices_position[6 * i + 3] = glm::vec2(w.map[i].x, w.map[i].y - (2 / w.size.y));
-        vertices_color[6 * i + 3] = tileColor;
+		vertices_position[6 * i + 3] = glm::vec2(w.map[i].x, w.map[i].y - (2 / w.size.y));
+		vertices_color[6 * i + 3] = tileColor;
 
-        vertices_position[6 * i + 4] = glm::vec2(w.map[i].x + (2 / w.size.x), w.map[i].y);
-        vertices_color[6 * i + 4] = tileColor;
+		vertices_position[6 * i + 4] = glm::vec2(w.map[i].x + (2 / w.size.x), w.map[i].y);
+		vertices_color[6 * i + 4] = tileColor;
 
-        vertices_position[6 * i + 5] = glm::vec2(w.map[i].x + (2 / w.size.x), w.map[i].y - (2 / w.size.y));
-        vertices_color[6 * i + 5] = tileColor;
-    }
+		vertices_position[6 * i + 5] = glm::vec2(w.map[i].x + (2 / w.size.x), w.map[i].y - (2 / w.size.y));
+		vertices_color[6 * i + 5] = tileColor;
+	}
 
-    shaderProgram = create_program("./shaders/vertex.vert", "./shaders/fragment.frag");
+	shaderProgram = create_program("./shaders/vertex.vert", "./shaders/fragment.frag");
 
-    textureShaderProg = create_program("./shaders/vertexTex.vert", "./shaders/fragmentTex.frag");
+	textureShaderProg = create_program("./shaders/vertexTex.vert", "./shaders/fragmentTex.frag");
 
 	// Load texture
 	GLuint texture;
@@ -162,14 +162,14 @@ void setupOpengl() {
 		glm::vec2(pm.position.x + (2 / w.size.y) * 15, pm.position.y),
 		glm::vec2(pm.position.x,  pm.position.y + (2 / w.size.y) * 15),
 		glm::vec2(pm.position.x + (2 / w.size.y) * 15, pm.position.y) ,
-		glm::vec2(pm.position.x + (2 / w.size.y) * 15,  pm.position.y + (2 / w.size.y)*15),
+		glm::vec2(pm.position.x + (2 / w.size.y) * 15,  pm.position.y + (2 / w.size.y) * 15),
 	};
 
 	GLfloat texVertices[] = {
-		pm.position.x, pm.position.y, 1.0f, 1.0f, 1.0f, 0.025f, 1.0f,													// 0,1
-		pm.position.x,  pm.position.y + (2 / w.size.y) * 15, 1.0f, 1.0f, 1.0f, 0.0f, 0.1f,								// 0,0
-		pm.position.x + (2 / w.size.y) * 15, pm.position.y, 1.0f, 1.0f, 1.0f, 0.1f, 1.0f,								// 1,0
-		pm.position.x + (2 / w.size.y) * 15,  pm.position.y + (2 / w.size.y) * 15, 1.0f, 1.0f, 1.0f, 0.1f, 0.1f,		// 1,1
+		pm.position.x, pm.position.y, 1.0f, 1.0f, 1.0f, 0.01f, 1.0f,														// 0,1
+		pm.position.x,  pm.position.y + (2 / w.size.y) * 15, 1.0f, 1.0f, 1.0f, 0.0f, 0.2f,								// 0,0
+		pm.position.x + (2 / w.size.y) * 15, pm.position.y, 1.0f, 1.0f, 1.0f, 0.2f, 1.0f,								// 1,1
+		pm.position.x + (2 / w.size.y) * 15,  pm.position.y + (2 / w.size.y) * 15, 1.0f, 1.0f, 1.0f, 0.2f, 0.2f,		// 1,0
 	};
 
 	GLuint order[] = {
@@ -177,46 +177,46 @@ void setupOpengl() {
 		1, 2, 3
 	};
 
-	
-    glGenVertexArrays(1, &vaoMap);
 
-    glBindVertexArray(vaoMap);
+	glGenVertexArrays(1, &vaoMap);
 
-    glGenBuffers(NUM_BUFFERS, vbo);
+	glBindVertexArray(vaoMap);
 
-    // !!!! Make a buffer for VERTEX POSITIONS !!!!
-    // Bind the buffer
-    glBindBuffer(GL_ARRAY_BUFFER, vbo[VB_POSITION]);
+	glGenBuffers(NUM_BUFFERS, vbo);
 
-    // Put the given data in buffer
-    glBufferData(GL_ARRAY_BUFFER, w.size.x * w.size.y * 6 * sizeof(glm::vec2), &vertices_position[0], GL_STATIC_DRAW);
+	// !!!! Make a buffer for VERTEX POSITIONS !!!!
+	// Bind the buffer
+	glBindBuffer(GL_ARRAY_BUFFER, vbo[VB_POSITION]);
 
-    // Enable buffer 'VB_POSITION' for use
-    glEnableVertexAttribArray(VB_POSITION);
+	// Put the given data in buffer
+	glBufferData(GL_ARRAY_BUFFER, w.size.x * w.size.y * 6 * sizeof(glm::vec2), &vertices_position[0], GL_STATIC_DRAW);
 
-    // Tell OpenGL how to use the enabled buffer
-    glVertexAttribPointer(VB_POSITION, 2, GL_FLOAT, GL_FALSE, 0, 0);
+	// Enable buffer 'VB_POSITION' for use
+	glEnableVertexAttribArray(VB_POSITION);
 
-    // !!!! Make a buffer for the COLORS !!!!
-    // Bind the buffer
-    glBindBuffer(GL_ARRAY_BUFFER, vbo[VB_COLOR]);
+	// Tell OpenGL how to use the enabled buffer
+	glVertexAttribPointer(VB_POSITION, 2, GL_FLOAT, GL_FALSE, 0, 0);
 
-    // Put the given data in buffer
-    glBufferData(GL_ARRAY_BUFFER, w.size.x * w.size.y * 6 * 16, &vertices_color[0], GL_STATIC_DRAW);
+	// !!!! Make a buffer for the COLORS !!!!
+	// Bind the buffer
+	glBindBuffer(GL_ARRAY_BUFFER, vbo[VB_COLOR]);
 
-    // Enable buffer 'VB_COLOR' for use
-    glEnableVertexAttribArray(VB_COLOR);
+	// Put the given data in buffer
+	glBufferData(GL_ARRAY_BUFFER, w.size.x * w.size.y * 6 * 16, &vertices_color[0], GL_STATIC_DRAW);
 
-    // Tell OpenGL how to use the enabled buffer
-    glVertexAttribPointer(VB_COLOR, 4, GL_FLOAT, GL_FALSE, 0, 0);
+	// Enable buffer 'VB_COLOR' for use
+	glEnableVertexAttribArray(VB_COLOR);
+
+	// Tell OpenGL how to use the enabled buffer
+	glVertexAttribPointer(VB_COLOR, 4, GL_FLOAT, GL_FALSE, 0, 0);
 
 	/* PACMAN, GHOSTS and Special food*/
 
-    glGenVertexArrays(1, &vaoObj);
+	glGenVertexArrays(1, &vaoObj);
 
-    glBindVertexArray(vaoObj);
+	glBindVertexArray(vaoObj);
 
-    glGenBuffers(1, &vboObj);
+	glGenBuffers(1, &vboObj);
 
 	glBindBuffer(GL_ARRAY_BUFFER, vboObj);
 
@@ -230,22 +230,22 @@ void setupOpengl() {
 
 	glGenBuffers(1, &veoObj);
 
-    // !!!! Make a buffer for VERTEX POSITIONS !!!!
-    // Bind the buffer
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, veoObj);
+	// !!!! Make a buffer for VERTEX POSITIONS !!!!
+	// Bind the buffer
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, veoObj);
 
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(GLuint)*3*2, NULL, GL_STATIC_DRAW);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(GLuint) * 3 * 2, NULL, GL_STATIC_DRAW);
 	glBufferSubData(GL_ELEMENT_ARRAY_BUFFER, 0, sizeof(order), &order);
 
-	
-    // Put the given data in buffer
-    //glBufferData(GL_ARRAY_BUFFER, sizeof(pacman_vertices) + sizeof(texCoord), NULL, GL_STATIC_DRAW);
+
+	// Put the given data in buffer
+	//glBufferData(GL_ARRAY_BUFFER, sizeof(pacman_vertices) + sizeof(texCoord), NULL, GL_STATIC_DRAW);
 
 	//glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(pacman_vertices), &pacman_vertices);
-	
+
 	//glBufferSubData(GL_ARRAY_BUFFER, sizeof(pacman_vertices), sizeof(texCoord), &texCoord);
 
-	
+
 
 	// Enable buffer 'VB_POSITION' for use
 	glEnableVertexAttribArray(VB_POSITION);
@@ -266,14 +266,14 @@ void setupOpengl() {
 
 void display() {
 
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-    glUseProgram(shaderProgram);
-    glBindVertexArray(vaoMap);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glUseProgram(shaderProgram);
+	glBindVertexArray(vaoMap);
 
 
 
-    // GL_TRIANGLE_STRIP WAS THE PROBLEM
-    glDrawArrays(GL_TRIANGLES, 0, 6 * w.size.x * w.size.y);
+	// GL_TRIANGLE_STRIP WAS THE PROBLEM
+	glDrawArrays(GL_TRIANGLES, 0, 6 * w.size.x * w.size.y);
 
 	glUseProgram(textureShaderProg);
 
@@ -290,44 +290,44 @@ void display() {
 	glBindVertexArray(vaoObj);
 
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, veoObj);
-	
+
 	glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, (const GLvoid*)0);
 
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
 
-    glBindVertexArray(0);
-    glUseProgram(0);
+	glBindVertexArray(0);
+	glUseProgram(0);
 }
 
 
 
 
 int main() {
-    readFile();
+	readFile();
 
-    setupOpengl();
-    // Display the map on window	
+	setupOpengl();
+	// Display the map on window	
 
-    do {
+	do {
 
-        //glDisableVertexAttribArray(0);
-        glfwPollEvents();
+		//glDisableVertexAttribArray(0);
+		glfwPollEvents();
 
-        glClearColor(0.5f, 1.0f, 1.0f, 0.0f);
+		glClearColor(0.5f, 1.0f, 1.0f, 0.0f);
 
-        display();
+		display();
 
-        glfwSwapBuffers(window);
-
-
-    } while (glfwGetKey(window, GLFW_KEY_ESCAPE) != GLFW_PRESS &&
-        glfwWindowShouldClose(window) == 0);
+		glfwSwapBuffers(window);
 
 
-    glfwDestroyWindow(window);
-    glfwTerminate();
-    return 0;
+	} while (glfwGetKey(window, GLFW_KEY_ESCAPE) != GLFW_PRESS &&
+		glfwWindowShouldClose(window) == 0);
+
+
+	glfwDestroyWindow(window);
+	glfwTerminate();
+	return 0;
 }
 
 
@@ -396,15 +396,15 @@ glm::vec2(0.5f, 1.0f),	//top right -> lower right
 /*
 
 glm::vec2 texCoord[] = {
-	// coord		texture
-	glm::vec2(0.0f, 0.0f),	//lower left -> top left
-	glm::vec2(0.0f, 1.0f),	//top left -> lower left
-	glm::vec2(1.0f, 0.0f),	//lower right -> top right
+// coord		texture
+glm::vec2(0.0f, 0.0f),	//lower left -> top left
+glm::vec2(0.0f, 1.0f),	//top left -> lower left
+glm::vec2(1.0f, 0.0f),	//lower right -> top right
 
 
-	glm::vec2(0.0f, 1.0f),	//top-left -> lower left
-	glm::vec2(1.0f, 0.0f),	//lower right -> top right
-	glm::vec2(1.0f, 1.0f),	//top right -> lower right
+glm::vec2(0.0f, 1.0f),	//top-left -> lower left
+glm::vec2(1.0f, 0.0f),	//lower right -> top right
+glm::vec2(1.0f, 1.0f),	//top right -> lower right
 };
 
 */
